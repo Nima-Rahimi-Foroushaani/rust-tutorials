58dbc225f8cc5753afdcbea444babe16460edf44
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2021-03-07 14:57:16 +0100

    Descriptions for references project
    Some comments as descriptions for this erroneous code

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
     let mut v:Vec<i32> = vec![1,2,3,4,5];
+    // first mutable reference
     let r1 = &mut v;
     let len1 = r1.len();
 
+    // second mutable reference - overlaps with the scope of the first -> Error
     let r2 = &mut v;
     r2.pop().expect("Panic!");
     
+    // Below for may be a motivation for applying mutable reference constraints even in a single thread
     for i in 0..len1 {
         println!("{}", r1[i]);
     }
