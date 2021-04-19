5a095f7571fe1ec8a68c8945b4f0c534da691433
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2021-04-19 09:20:36 +0200

    Progress

--- doc_examples/variable_states/src/borrow.rs
+++ doc_examples/variable_states/src/borrow.rs
@@ -1,1 +1,27 @@
 #![allow(unused)]
+
+pub fn f1() {
+    let x = 42;
+    let rx = &x;
+    let y = x + 1;
+    println!("{}", rx);
+}
+
+pub fn f2() {
+    let mut x = 42;
+    let rx = &x;
+    let y = x + 1;
+    //x = y;
+    //error[E0506]: cannot assign to `x` because it is borrowed
+    println!("{}", rx);
+}
+
+pub fn f3() {
+    let mut x = 42;
+    let rx = &mut x;
+    //x = 43;
+    //error[E0506]: cannot assign to `x` because it is borrowed
+    //let y = x + 1;
+    //error[E0503]: cannot use `x` because it was mutably borrowed
+    println!("{}", rx);
+}
