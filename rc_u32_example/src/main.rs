e569d1e8ddfdc0bc1e07fefc8beefb4a316166ba
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2021-12-14 19:22:32 +0100

    Progress

---
+++ rc_u32_example/src/main.rs
@@ -0,0 +1,31 @@
+mod stde;
+
+#[allow(unused)]
+fn main() -> () {
+    let rc1 = stde::rc_u32::RcU32::new(42);
+    {
+        let imm_rc1 = &rc1;
+        let rc2 = stde::rc_u32::RcU32::clone(imm_rc1);
+    }
+    let rc3 = rc1; // move
+}
+/***
+ * private safe fn main<|>() -> own unit {
+ * let *num = 42;
+ * let rc1 = stde::rc_u32::new<>(num);
+ *
+ * intro alpha;
+ * let imm_rc1 = mutbor'alpha rc1;
+ * immut imm_rc1;
+ * let rc2 = stde::rc_u32::clone<alpha>(imm_rc1);
+ * drop rc2;
+ * now alpha;
+ *
+ * let *o_rc1 = rc1;
+ * let rc3 = *o_rc1;
+ * drop rc3;
+ *
+ * let *r = unit{};
+ * return r;
+ * }
+ */
---
+++ rc_u32_example/src/stde.rs
@@ -0,0 +1,6 @@
+/**
+ * for this version of calculus without multithreading there is no need for implementing !Sync
+ * to prevent instances from being send to other threads
+ */
+pub mod cell_u32;
+pub mod rc_u32;
