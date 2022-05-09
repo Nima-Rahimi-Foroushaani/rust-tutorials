8a397efc2ca21d84aaeba596badb34bcf91f651a
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2022-05-09 17:03:03 +0200

    Upload

---
+++ doc_examples/predoc_report/src/deque.rs
@@ -0,0 +1,16 @@
+pub struct Node {
+    prev: *mut Node,
+    value: i32,
+    next: *mut Node,
+}
+
+pub unsafe fn create_deque() -> *mut Node {
+    let sentinel: *mut Node = std::alloc::alloc(std::alloc::Layout::new::<Node>()) as *mut Node;
+    addr_of_mut!((*sentinel).prev).write(sentinel);
+    addr_of_mut!((*sentinel).next).write(sentinel);
+    return sentinel;
+}
+
+pub unsafe fn is_empty(deque: *mut Node) -> bool {
+    return (*deque).next == deque;
+}
