5c078438acf74fd7b89e93cfe6d91396514ff368
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2022-05-27 11:13:27 +0200

    Up

--- doc_examples/predoc_report/src/rust_access_type.rs
+++ doc_examples/predoc_report/src/rust_access_type.rs
@@ -1,37 +1,40 @@
-pub fn access_types()
-{
+pub fn push_four(v: &mut Vec<i32>) {
+    v.push(4)
+}
+
+pub fn access_types() {
     let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
     {
-        let mut_ref: &mut Vec<i32> = &mut v;
+        let mrv: &mut Vec<i32> = &mut v;
         /***
-         * mut_ref is a mutable borrow of v
+         * mrv is a mutable borrow of v
          * as long as this borrow is alive it
          * is not possible to access the vector through v
          */
-        mut_ref.push(4); // mutable borrow has full access
+        push_four(mrv); // mutable borrow has full access
     }
 
     let _ = v.pop(); // v has its ownership back
 
     {
-        let shr_ref: &Vec<i32> = &v;
+        let srv: &Vec<i32> = &v;
         /***
-         * shr_ref is a shared/immutable borrow of v
+         * srv is a shared/immutable borrow of v
          * the vector cannot get mutated as long as
          * it is borrowed by any immutable borrow
          */
         {
             let first: &i32 = v.first().unwrap();
             /***
-             * multiple shared references borrowing from
-             * the same owner can coexist
+             * multiple shared references, borrowing from
+             * the same owner, can coexist
              */
-            println!("{} is the first in {:?}", first, shr_ref);
+            println!("{} is the first in {:?}", first, srv);
         }
     }
     let _ = v.pop();
     /***
-     * The owner, v goes out of scope here
+     * The owner v goes out of scope here
      * and the value gets dropped
      */
 }
