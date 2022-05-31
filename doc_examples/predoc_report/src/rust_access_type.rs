f98437e0aed0a2d246f538b45b5820db771206c0
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2022-05-31 15:11:33 +0200

    Up

--- doc_examples/predoc_report/src/rust_access_type.rs
+++ doc_examples/predoc_report/src/rust_access_type.rs
@@ -1,14 +1,14 @@
 pub fn push_four<'a>(r: &'a mut Vec<i32>) {
     r.push(4)
 }
-
+/*** [k] means the k's lifetime */
 pub fn access_types() {
     let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
     {//----------------------------------------------------
         let mrv: &mut Vec<i32> = &mut v; //               |
         /***                                              |
          * mrv is a mutable borrow of v                   |
-         * as long as this borrow is alive it        [mrv lifetim]
+         * as long as this borrow is alive it           [mrv]
          * is not possible to access                      |
          * the vector through v                           |
          */ //                                            |
@@ -17,22 +17,25 @@ pub fn access_types() {
 
     let _ = v.pop(); // v has its ownership back
 
-    {
-        let srv: &Vec<i32> = &v;
-        /***
-         * srv is a shared/immutable borrow of v
-         * the vector cannot get mutated as long as
-         * it is borrowed by any immutable borrow
-         */
-        {
-            let first: &i32 = v.first().unwrap();
-            /***
-             * multiple shared references, borrowing from
-             * the same owner, can coexist
-             */
-            println!("{} is the first in {:?}", first, srv);
-        }
-    }
+    {//----------------------------------------------------
+        let srv: &Vec<i32> = &v; //                       |
+        /***                                              |
+         * srv is a shared/immutable borrow of v          |
+         * the vector cannot get mutated as long as       |
+         * it is borrowed by any immutable borrow         |
+         */ //                                            |
+        {//----------------------------------------       |
+            let first: &i32 = //                  |       |
+                v.first().unwrap(); //            |       |
+            /***                                  |     [srv]
+             * multiple shared references,        |       |
+             * borrowing from the same owner,     |       |
+             * can coexist                     [first]    |
+             */ //                                |       |
+            println!("{} is the first in {:?}", //|       |
+                first, srv); //                   |       |
+        }//----------------------------------------       |
+    }//----------------------------------------------------
     let _ = v.pop();
     /***
      * The owner v goes out of scope here
