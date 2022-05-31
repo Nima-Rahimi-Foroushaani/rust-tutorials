4d7127bce3321b889c06a517d83ac768a5b5618c
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2022-05-31 17:10:11 +0200

    Up

--- doc_examples/predoc_report/src/rust_access_type_shr_slide.rs
+++ doc_examples/predoc_report/src/rust_access_type_shr_slide.rs
@@ -1,16 +1,3 @@
-/*** [l1] means the lifetime l1 */
-pub fn exclusive_own() {
-    let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
-    {/***********************[l1]*************************/
-        let mrv: &mut Vec<i32> = &mut v; // under [l1]    |
-        push_four(mrv); //                                |
-    }/****************************************************/
-    let _ = v.pop(); // v has its ownership back
-}
-pub fn push_four<'a>(r: &'a mut Vec<i32>) {
-    r.push(4)
-}
-
 pub fn shared_own() {
     let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
     {/***********************[l2]*************************/
