0c83b5f404e720330dbcdb03aeef2defc11f8432
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2022-05-31 17:04:29 +0200

    Up

--- doc_examples/predoc_report/src/rust_access_type_slide.rs
+++ doc_examples/predoc_report/src/rust_access_type_slide.rs
@@ -1,16 +1,18 @@
-pub fn push_four<'a>(r: &'a mut Vec<i32>) {
-    r.push(4)
-}
 /*** [l1] means the lifetime l1 */
-pub fn access_types() {
+pub fn exclusive_own() {
     let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
     {/***********************[l1]*************************/
         let mrv: &mut Vec<i32> = &mut v; // under [l1]    |
         push_four(mrv); //                                |
     }/****************************************************/
-
     let _ = v.pop(); // v has its ownership back
+}
+pub fn push_four<'a>(r: &'a mut Vec<i32>) {
+    r.push(4)
+}
 
+pub fn shared_own() {
+    let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
     {/***********************[l2]*************************/
         let srv: &Vec<i32> = &v; // under [l2]            |
         {/*******************[l3]*****************/       |
@@ -21,8 +23,4 @@ pub fn access_types() {
         }/****************************************/       |
     }/****************************************************/
     let _ = v.pop(); // v has its ownership back
-    /***
-     * The owner v goes out of scope
-     * vector gets dropped
-     */
 }
