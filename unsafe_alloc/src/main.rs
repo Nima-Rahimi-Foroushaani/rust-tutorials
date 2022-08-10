26c8b9aad467c020fd33ee4a7a14ad8b64503b74
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2022-08-10 16:23:58 +0200

    Up

--- unsafe_alloc/src/main.rs
+++ unsafe_alloc/src/main.rs
@@ -1,14 +1,21 @@
 fn main()
-   /*@ requires true;
-   
+/*@ requires true;
+
 
 @*/
 // Just a happy comment
 //@ ensures true;
 {
     //@ open somthing();
-    // unsafe {
-    //     let layout = std::alloc::Layout::new::<i8>();
-    //     let _p = std::alloc::alloc(layout) as *mut i8;
-    // }
+    unsafe {
+        let layout = std::alloc::Layout::new::<u8>();
+        // let _p = std::alloc::alloc(layout) as *mut u8;
+    }
 }
+
+// fn function1(i: u8)
+// //@ requires true;
+// //@ ensures true;
+// {
+//     let j: u8 = 42;
+// }
