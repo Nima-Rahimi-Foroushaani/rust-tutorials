17753a97fa916d45609df4c36a7d81a26f06b804
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2023-02-28 17:34:42 +0100

    Up

--- doc_examples/predoc_report/src/rust_access_type_shr_slide.rs
+++ doc_examples/predoc_report/src/rust_access_type_shr_slide.rs
@@ -1,13 +1,13 @@
 pub fn shared_own() {
-    let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
-    {/***********************[l2]*************************/
-        let srv: &/*'l2*/Vec<i32> = &v;//                 |
-        {/*******************[l3]*****************///     |
-            let first: &/*'l3*/i32 =//            |       |
-                v.first().unwrap(); //            |       |
+    let mut v: Vec<i32> = vec![1, 2, 3];// v is the owner
+    {/***********************[l1]*************************/
+        let srv: &/*'l1*/Vec<i32> = &v;//                 |
+        {/*******************[l2]*****************///     |
+            let first: &/*'l2*/i32 =//            |       |
+                v.first().unwrap();//             |       |
             println!("{} is the first in {:?}",// |       |
-                first, srv); //                   |       |
+                first, srv);//                    |       |
         }/****************************************///     |
     }/****************************************************/
-    let _ = v.pop(); // v has its ownership back
+    let _ = v.pop();// v has its ownership back
 }
--- doc_examples/predoc_report/src/rust_access_type_texc_slide.rs
+++ doc_examples/predoc_report/src/rust_access_type_texc_slide.rs
@@ -1,10 +1,10 @@
 pub fn temp_exclusive_own() {
-    let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
+    let mut v: Vec<i32> = vec![1, 2, 3];// v is the owner
     {/***********************[l1]*************************/
         let mrv: &/*'l1*/mut Vec<i32> = &mut v;//         |
-        push_four(mrv); //                                |
+        push_four(mrv);//                                 |
     }/****************************************************/
-    let _ = v.pop(); // v has its ownership back
+    let _ = v.pop();// v has its ownership back
 }
 pub fn push_four<'a>(r: &'a mut Vec<i32>) {
     r.push(4)
