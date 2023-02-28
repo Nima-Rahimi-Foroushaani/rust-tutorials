4d93485d0fe2430092c31d96b2c8e826b3ea3c11
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2023-02-28 17:06:20 +0100

    Up

--- doc_examples/predoc_report/src/rust_access_type_texc_slide.rs
+++ doc_examples/predoc_report/src/rust_access_type_texc_slide.rs
@@ -1,7 +1,7 @@
 pub fn temp_exclusive_own() {
     let mut v: Vec<i32> = vec![1, 2, 3]; // v is the owner
     {/***********************[l1]*************************/
-        let mrv: &/*'l1*/mut Vec<i32> = &mut v;           |
+        let mrv: &/*'l1*/mut Vec<i32> = &mut v;//         |
         push_four(mrv); //                                |
     }/****************************************************/
     let _ = v.pop(); // v has its ownership back
