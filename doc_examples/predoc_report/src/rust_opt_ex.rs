488abbfe7cd3403f5ab31fc753a740df39ee73da
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2023-04-20 13:08:48 +0200

    Up

--- doc_examples/predoc_report/src/rust_opt_ex.rs
+++ doc_examples/predoc_report/src/rust_opt_ex.rs
@@ -1,12 +1,12 @@
 use std::cell::Cell;
 
-fn do_somthing(c: &Cell<u32>, u: u32) {
+fn do_something(c: &Cell<u32>, u: u32) {
     c.set(u)
 }
 
 fn f<'a>(c: &'a Cell<u32>) {
     for i in 0..100 {
-        do_somthing(c, i);
+        do_something(c, i);
         println!("{:?}", *c)
     }
 }
