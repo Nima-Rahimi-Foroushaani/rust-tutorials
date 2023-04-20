a2a263f64cee26442e6cbdd4e7ff695a1eb92ee6
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2023-04-20 20:14:20 +0200

    Up

--- doc_examples/predoc_report/src/unsafe_mem_alloc.rs
+++ doc_examples/predoc_report/src/unsafe_mem_alloc.rs
@@ -1,26 +1,28 @@
+use std::alloc::{alloc, dealloc, Layout};
+use std::process::abort;
+
 fn caller() {
-    let layout = std::alloc::Layout::new::<u8>();
+    let layout = Layout::new::<u8>();
     unsafe {
         let p = alloc_u8();
         *p = 42;
         if *p != 42 {
-            std::alloc::dealloc(p, layout);
-            std::alloc::dealloc(p, layout);
+            dealloc(p, layout);
+            dealloc(p, layout);
         }
-        std::alloc::dealloc(p, layout);
+        dealloc(p, layout);
     }
 }
 
 unsafe fn alloc_u8() -> *mut u8
 // Function Specification
 //@ requires true;
-/*@ ensures integers__(result, 1, false, 1, _) &*& malloc_block(result, 1) &*&
-object_pointer_within_limits(result, 1) == true; @*/ {
+//@ ensures result[..1] |-> _ &*& malloc_block(result, 1);
+{
     unsafe {
-        let layout = std::alloc::Layout::new::<u8>();
-        let p = std::alloc::alloc(layout);
+        let p = alloc(Layout::new::<u8>());
         if p.is_null() {
-            std::alloc::handle_alloc_error(layout);
+            abort();
         }
         p
     }
