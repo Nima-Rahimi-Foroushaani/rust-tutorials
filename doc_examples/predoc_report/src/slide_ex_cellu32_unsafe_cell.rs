0933d004d6aebaf90491bf60713b6ebf026366d7
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2023-04-22 16:54:35 +0200

    Up

--- doc_examples/predoc_report/src/slide_ex_cellu32_unsafe_cell.rs
+++ doc_examples/predoc_report/src/slide_ex_cellu32_unsafe_cell.rs
@@ -5,13 +5,14 @@ pub struct Cell<T: ?Sized> { value: UnsafeCell<T> }
 
 impl<T> Cell<T> {
     pub const fn new(value: T) -> Cell<T> {
-        Cell { value: UnsafeCell::new(value) } }
+        Cell { value: UnsafeCell::new(value) }}
+
     pub fn replace(&self, val: T) -> T {
-        mem::replace(unsafe { &mut *self.value.get() }, val) }
+        mem::replace(unsafe { &mut *self.value.get() }, val)}
 }
 impl<T: Copy> Cell<T> {
     pub fn get(&self) -> T {
-        unsafe { *self.value.get() } }
+        unsafe { *self.value.get() }}
 }
 
 impl<T: ?Sized> !Sync for Cell<T> {}
