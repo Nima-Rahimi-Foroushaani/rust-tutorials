dc1ccd7f077cb21813016ccc595ead19c5e97c54
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2023-04-22 17:42:38 +0200

    Up

--- doc_examples/predoc_report/src/slide_ex_cellu32.rs
+++ doc_examples/predoc_report/src/slide_ex_cellu32.rs
@@ -3,9 +3,9 @@ struct CellU32 { v: u32 }
 impl CellU32 {
   pub fn new(u: u32) -> CellU32 { CellU32 { v: u } }
 
-  pub fn get<'a>(&'a self) -> u32 { self.v }
+  pub fn get(&self) -> u32 { self.v }
 
-  fn set<'a>(&'a self, u: u32) {
+  fn set(&self, u: u32) {
     let p = &self.v as *const u32 as *mut u32;
     unsafe { *p = u; }
   }
